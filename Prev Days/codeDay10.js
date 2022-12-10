6447e5c36599c8739b79a7af1315c93ed94d6ba5
Yayroos <yayroos@protonmail.com>
2022-12-10 18:30:47 +1100

    day 10, also no longer committing inputs because apparently were not supposed to do that

---
+++ Prev Days/codeDay10.js
@@ -0,0 +1,43 @@
+import input from './input.js';
+
+let splitinput = input.split('\n');
+
+let xReg = 1;
+
+let clock = 0;
+
+let outString = '';
+
+function incrementClock(){
+    render();
+    clock ++;
+    if(clock <= 220 && clock % 40 === 0){
+        outString += '\n';
+    }
+}
+
+function updatexReg(val){
+    xReg += val;
+}
+
+function render(){
+    let drawingCol = clock % 40;
+    if(Math.abs(drawingCol - xReg) <= 1){
+        outString += '#';
+    } else {
+        outString += '.';
+    }
+}
+
+splitinput.forEach(instruction => {
+    if(instruction === 'noop'){
+        incrementClock();
+    } else {
+        let change = parseInt(instruction.split(' ')[1]);
+        incrementClock();
+        incrementClock();
+        updatexReg(change);
+    }
+})
+
+console.log(outString);
